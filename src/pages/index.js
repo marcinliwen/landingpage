15dd1ba20d5f5fdef31be9b9dd3561004e6b00f2
liwenmarcin <marcin.liwen@sohoshop.pl>
2021-09-28 15:02:57 +0200

    ready!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,277 @@
 import * as React from "react"
+import "./style.css"
+import { StaticImage } from "gatsby-plugin-image"
+import BoxIcon from "../assets/open-box.svg"
+import FastIcon from "../assets/fast-time.svg"
+import SupportIcon from "../assets/conversation.svg"
+import Ceo from "../images/persons/ceo.png"
+import Manager from "../images/persons/manager.png"
+import TestimonialFace from "../images/testimonial.png"
+import { InView } from 'react-intersection-observer';
+import CountUp from 'react-countup';
+import ContactForm from '../components/ContactForm'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+const tabHandler =()=>{
+  
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <>
+    <main>
+      <section className="bg-gradient section-home">
+        <div className="container">
+        <div className="row wrap">
+          <div className="col-12 col-md-6">
+            <p className="upper-title">Strona Lading Page</p>
+            <h1 className="title">Nazwa produktu lub usługi </h1>
+            <p className="subtitle">W tym pudełku jest wszystko czego potrzebujesz, jeszcze nie raz Cię zaskoczy. </p>
+            <button className="cfa-btn">Zamawiam</button>
+          </div>
+          <div className="col-12 col-md-6">
+            <StaticImage 
+            src="../images/product_nobg.png" 
+            alt="product" 
+            placeholder="none"
+            />
+          </div>
+        </div>
+        </div>
+      </section>
+      <section id="partners" className="bg-dark">
+        <div className="container">
+          <div className="row mobile-slider">
+            <div className="col-3 col-md-2">
+              <div className="partners-item">
+                PARTNER
+              </div>
+            </div>
+            <div className="col-3 col-md-2">
+              <div className="partners-item">
+                PARTNER
+              </div>
+            </div>
+            <div className="col-3 col-md-2">
+              <div className="partners-item">
+                PARTNER
+              </div>
+            </div>
+            <div className="col-3 col-md-2">
+              <div className="partners-item">
+                PARTNER
+              </div>
+            </div>
+            <div className="col-3 col-md-2">
+              <div className="partners-item">
+                PARTNER
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-dark">
+        <div className="container">
+        <div className="row wrap features-icon">
+          <div className="col-12 col-md-4">
+            <div className="row wrap features-item">
+              <BoxIcon fill="#fff" width="48px"  height="48px"/>
+              <p className="col-12"><b>Trwałe opakowanie</b></p>
+              <p className="col-12">Zapakowane w najlepszym opakowaniu na rynku!</p>
+            </div>
+          </div>
+          <div className="col-12 col-md-4">
+            <div className="row wrap features-item">
+              <FastIcon fill="#fff" width="48px"  height="48px"/>
+              <p className="col-12"><b>Najszybszy transport</b></p>
+              <p className="col-12">Najszybszy transport z dostawą do domu!</p>
+            </div>
+          </div>
+          <div className="col-12 col-md-4">
+            <div className="row wrap features-item">
+              <SupportIcon fill="#fff" width="48px"  height="48px"/>
+              <p className="col-12"><b>Wsparcie specjalistów</b></p>
+              <p className="col-12">Wsparcie specjalistów na każdym kroku współpracy!</p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </section>
+      <section id="services">
+        <div className="container">
+        <div className="row wrap">
+          <div className="section-title col-md-12"> <h2>Nasz produkt</h2></div>
+          <div className="col-12 col-md-6">
+            <StaticImage 
+            src="../images/packs.png" 
+            alt="product" 
+            placeholder="none"
+            />
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="services-item">
+              <div className="services-title"><span>Pakowanie</span></div>
+              <div className="services-descs">Pakujemy wszystko i w każdym rozmiarze i w każdym stanie skupienia.</div>
+            </div>
+            <div className="services-item">
+              <div className="services-title"><span>Produkcja</span></div>
+              <div className="services-descs">Produkujemy najlepszej jakości pudełka, składane w dowolny sposób</div>
+            </div>
+            <div className="services-item">
+              <div className="services-title"><span>Dowóz</span></div>
+              <div className="services-descs">Dowieziemy pudełka we wskazany adres na terenie Pacanowa.</div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </section>
+      <section id="about">
+        <div className="container">
+          <div className="section-title"> <h2>Nasz zespół</h2></div>
+          <div className="row wrap about-wrapper">
+            <div className="col-12 col-md-3">
+              <div className="about-item">
+                <StaticImage 
+                  src="../images/persons/ceo.png"  
+                  alt="zdjęcie"
+                  />
+                  <p>CEO</p>
+                <h3>Jan Kowalski</h3>
+                
+              </div>
+            </div>
+            <div className="col-12 col-md-3">
+              <div className="about-item">
+                <StaticImage 
+                  src="../images/persons/manager.png" 
+                  alt="zdjęcie"
+                  placeholder="none"
+                  />
+                  <p>Manager</p>
+                <h3>Joanna Malinowska</h3>
+                
+              </div>
+            </div>
+            <div className="col-12 col-md-3">
+              <div className="about-item">
+                <StaticImage 
+                  src="../images/persons/project.png" 
+                  alt="zdjęcie"
+                  placeholder="none"
+                  />
+                  <p>Projektant</p>
+                <h3>Andrzej Kowalski</h3>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="counter">
+        <div className="container">
+        <div className="section-title"> <h2>W liczbach</h2></div>
+        <InView  delay={300} triggerOnce	>
+            {({ inView, ref, entry }) => (
+            <div className="row wrap" ref={ref}>
+                
+            <div className="col-12 col-md-4" >
+              <div className="counter-item">
+                <CountUp start={0} end={340} duration={6}/>
+                <p>Projektów opakowań</p></div>
+            </div>
+            <div className="col-12 col-md-4" >
+              <div className="counter-item">
+                <CountUp start={0} end={1340} duration={7}/>
+                <p>Zadowolonych klientów</p></div>
+            </div>
+            <div className="col-12 col-md-4" >
+              <div className="counter-item">
+                <CountUp start={0} end={40} duration={7}/>
+                <p>Pracowników</p></div>
+            </div>
+            </div>
+            )}
+        </InView>
+        </div>
+      </section>
+      <section id="testimonials">
+        <div className="container">
+        <div className="section-title"> <h2>Co o nas mówią?</h2></div>
+        <div className="testimonials-wrapper">
+          <ul className="testimonials-list">
+              <li className="testimonials-item">
+                <div className="testimonials-text">
+                  <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</q>
+                  <div className="testimonials-desc">
+                    <img className="testimonial-img" src={TestimonialFace} alt="Jan Wiśniewski" />                   
+                    <p className="testimonials-author">Jan Wiśniewski</p>
+                    <p className="testimonials-role">CEO My Company</p>
+                  </div>
+                </div>
+              </li>
+              <li className="testimonials-item">
+                <div className="testimonials-text">
+                  <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</q>
+                  <div className="testimonials-desc">
+                    <img className="testimonial-img" src={TestimonialFace} alt="Jan Wiśniewski" />                   
+                    <p className="testimonials-author">Jan Wiśniewski</p>
+                    <p className="testimonials-role">CEO My Company</p>
+                  </div>
+                  
+                </div>
+              </li>
+              <li className="testimonials-item">
+                <div className="testimonials-text">
+                  <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</q>
+                  <div className="testimonials-desc">
+                    <img className="testimonial-img" src={TestimonialFace} alt="Jan Wiśniewski" />                   
+                    <p className="testimonials-author">Jan Wiśniewski</p>
+                    <p className="testimonials-role">CEO My Company</p>
+                  </div>
+                </div>
+              </li>
+          </ul>
+        </div>
+        </div>
+      </section>
+      <section id="contact-form">
+        <div className="container">
+          <div className="row wrap">
+            <div className="section-title col-12"> <h2>Napisz do nas!</h2></div>
+            <div className="col-12 col-md-6">
+                <StaticImage 
+                src="../images/packs.png" 
+                alt="product" 
+                placeholder="none"
+                />
+            </div>
+            <div className="from-wrapper bg-dark col-12 col-md-6">
+                  <ContactForm />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+    <footer className="bg-dark">
+      <div className="container">
+        <div className="row wrap">
+          <div className="col-12 col-md-3">
+          <p className="upper-title">Strona Lading Page</p>
+            <h2 className="title">Nazwa produktu lub usługi </h2>
+            <p className="subtitle">W tym pudełku jest wszystko czego potrzebujesz, jeszcze nie raz Cię zaskoczy. </p>
+          </div>
+          <div className="col-12 col-md-3 address">
+              <p>Nazwa Firmy</p>
+              <p>Ulica 3</p>
+              <p>00-123 Miasto</p>
+              <p>adres@mailowy.pl</p>
+              <p>+48 500 600 700</p>
+          </div>
+          <div className="col-12 center">Made by Starywieloryb - 2021</div>
+        </div>
+      </div>
+    </footer>
+    </>
+   )
 }
 
 export default IndexPage
